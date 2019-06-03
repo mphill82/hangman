8c909b0eb3ad4105bc1cddf49ebd96fc64bd5598
mphill82 <mphillips2013@gmail.com>
2019-06-03 11:01:54 -0400

    error handling, game is running! just need display

--- theGame.js
+++ theGame.js
@@ -1,42 +1,50 @@
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var theWord;
+var guesses;
+var remainingLetterCount;
+var manCount;
+
 function reset(){
-    var theWord = "secret";
-    var guesses = "";
+    theWord = "secret";
+    guesses = "";
     remainingLetterCount = theWord.length;
     manCount = 6;
 }
 
 reset();
 
-while (1==1){
-    //LISTEN for a typed character, call it letter
-    //--> call the typed character letter
-    guesses+=letter;                                        //add users guess to the list of guessed letters
-    //--> adjust guessed letters display
-    var i;
-    var match = false;
+function userGuess(event){
+    var letter = event.key;
+    if (alphabet.includes(letter)&&!guesses.includes(letter)){              //we only continue if the key is new and a letter
+        guesses+=letter;                                                    //add users guess to the list of guessed letters
+        document.getElementById("previous_guesses").innerHTML = "Guesses: "+ guesses;    //display them                              
+        var i;
+        var match = false;
+        
 
-    //CHECK the guess letter by looping through each letter in theWord
-    for (i=0;i<theWord.length;i++){                         
-        if (letter===theWord.split("")[i]){                 //If the guessed letter matches a letter in theWord, we... 
-            match = true;                                   //verify a match, 
-            remainingLetterCount--;                         //descrease the remainingLetterCount,
-            //-->adjust display of theWord                  //and display the letter.
-            //-->change displayed message to "it's a match"
+        //CHECK the guess letter by looping through each letter in theWord
+        for (i=0;i<theWord.length;i++){                         
+            if (letter===theWord.split("")[i]){                             //If the guessed letter matches a letter in theWord, we... 
+                match = true;                                               //verify a match, 
+                remainingLetterCount--;                                     //descrease the remainingLetterCount,
+                //-->adjust display of theWord                              //and display the letter.
+                document.getElementById("message").innerHTML = "<h3>It's a match!</h3>";
+            }
         }
-    }
-    if (!match){                                            //If there were no matches, we... 
-        manCount--;                                         //decrease manCount and
-        //-->change the image of the man                    //change the image of the man.
-        //-->change displayed message to "no matches"
-    }
-
-    //END GAME?
-    if (remainingLetterCount<=0){                           //if youve won...
-        //-->change message display "win"
-        //--> make reset button which, if pressed, executes reset()
-    }
-    if (manCount<=0){                                       //if you've lost...
-        //-->change message display "lose"
-        //--> make reset button which, if pressed, executes reset()
+        if (!match){                                                        //If there were no matches, we... 
+            manCount--;                                                     //decrease manCount and
+            //-->change the image of the man                                //change the image of the man.
+            document.getElementById("message").innerHTML = "<h3>No match, sorry.</h3>";
+        }
+        console.log(match);
+        //END GAME?
+        if (remainingLetterCount<=0){                                       //if youve won...
+            document.getElementById("message").innerHTML = "<h3>You won! </h3>";
+            //--> make reset button which, if pressed, executes reset()
+        }
+        if (manCount<=0){                                                   //if you've lost...
+        document.getElementById("message").innerHTML = "<h3>You lost, sorry.</h3>";
+            //--> make reset button which, if pressed, executes reset()
+        }
     }
 }
